136a4ded63d763208b4c5b27f232f6cab6f02c92
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2016-08-04 10:45:39 +0200

    Config comentados

--- bo/xlsexport/config.php
+++ bo/xlsexport/config.php
@@ -1,17 +1,17 @@
 <?php
-define('Clavyuser','superuser');
-define('Clavyuserkey','Hs59YGxf');
-define('Clavykey','580115880326');
-define('ClavyServer','localhost');
-define('ClavyPort','8080');
-define('ClavyDomine','Clavy');
+define('Clavyuser','CLAVYUSERNAME');  //Nombre del usuario en Clavy
+define('Clavyuserkey','CLAVYUSERKEY'); //Contraseña del usuario en Clavy
+define('Clavykey','CLAVYPASSKEY'); //Clave de la Aplicacion (obtenida en Clavy)
+define('ClavyServer','CLAVYSERVER'); //Servidor de Clavy
+define('ClavyPort','CLAVYPORT'); //Puerto de Uso de Clavy
+define('ClavyDomine','CLAVYDOMAIN'); //Dominio de Clavy ( por defecto Clavy)
 
-define('TZN_ODA_HOST','localhost');
+define('TZN_ODA_HOST','ODASERVER'); //Servidor de OdA Actual
 
-define('TZN_DB_EXTERNAL_HOST','localhost');
+define('TZN_DB_EXTERNAL_HOST','MYSQLSERVER'); //Servidor de base de datos que ataca OdA
 
-define('TZN_DB_EXTERNAL_USER','root');       // Nombre del usuario de la base de datos
-define('TZN_DB_EXTERNAL_PASS','tbontb99');           // Contraseña del usuario de la base de datos
+define('TZN_DB_EXTERNAL_USER','MYSQLEXTERNALUSER');       // Nombre del usuario de la base de datos
+define('TZN_DB_EXTERNAL_PASS','MYSQLEXTERNALKAY');           // Contraseña del usuario de la base de datos
 
 set_time_limit(0);
 ?>
